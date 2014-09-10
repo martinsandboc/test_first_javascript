a6d45723f3aa4f90e427cc68f7022a05fa926171
Martin Butler <martinebutler@gmail.com>
2014-09-09 20:19:46 -0400

    change tabs to spaces

--- 02_loops/Loops.js
+++ 02_loops/Loops.js
@@ -1,52 +1,52 @@
 function repeat(str, num) {
-	var output = "";
-	while (num > 0) {
-		output += str;
-		num--;
-	}
-	return output;
+  var output = "";
+  while (num > 0) {
+    output += str;
+    num--;
+  }
+  return output;
 }
 
 function join (arr, delim) {
-	var output = "";
-	var arrLength = arr.length;
-	for (i=0; i < arrLength; i++) {
-		output += arr[i];
-		if ((delim != undefined) && (i != (arrLength -1))) {
-			output += delim;
-		}
-	}
-	return output;
+  var output = "";
+  var arrLength = arr.length;
+  for (i=0; i < arrLength; i++) {
+    output += arr[i];
+    if ((delim != undefined) && (i != (arrLength -1))) {
+      output += delim;
+    }
+  }
+  return output;
 }
 
 function sum (arrSum) {
-	var sumTotal = 0;
-	for (i=0; i < arrSum.length; i++) {
-		sumTotal += arrSum[i];
-	}
-	return sumTotal;
+  var sumTotal = 0;
+  for (i=0; i < arrSum.length; i++) {
+    sumTotal += arrSum[i];
+  }
+  return sumTotal;
 }
 
 function paramify (hash) {
-	var output = [];
-	for (var key in hash) {
-		if(hash.hasOwnProperty(key)) {
-			output.push(key + "=" + hash[key]);
-		}
-	}
-	output.sort();
-	stringOutput = output.join("&");
-	return stringOutput;
+  var output = [];
+  for (var key in hash) {
+    if(hash.hasOwnProperty(key)) {
+      output.push(key + "=" + hash[key]);
+    }
+  }
+  output.sort();
+  stringOutput = output.join("&");
+  return stringOutput;
 }
 
 function factorial (num) {
-	if (num == 0 || num == 1) {
-		return 1;
-	}
-	return num * (factorial(num-1));
+  if (num == 0 || num == 1) {
+    return 1;
+  }
+  return num * (factorial(num-1));
 }
 
 function concat_string () {
-	var args = Array.prototype.slice.call(arguments);
-	return args.join("");
+  var args = Array.prototype.slice.call(arguments);
+  return args.join("");
 }
